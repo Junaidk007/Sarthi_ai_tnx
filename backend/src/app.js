a120eb3b43589ad8a30538e79f5c4f7a9e717e00
Junaidk007 <junaid.k0004@gmail.com>
2026-07-26 14:51:43 +0530

    backend change

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,10 +9,7 @@ const app = express();
 
 // Core middlewares
 app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN || "*",
-    credentials: true,
-  })
+  cors()
 );
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
